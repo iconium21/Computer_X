2a3caba484e8d23e824a91f5bae53ae8ac5f630f
iconium21 <iconium6509@gmail.com>
2024-10-12 16:36:54 +0500

    Test

--- testfile01.cpp
+++ testfile01.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
+    //TO DO: Find a better way to this.
     int a=5;
     cout<<"Hello World\n";
-    cout<<"Value of a is:\t"; cout<<a; cout<<"\nEND";
+    cout<<"Value of a is:  "<< a <<"\nEND\n ";
     return 0;
 }
