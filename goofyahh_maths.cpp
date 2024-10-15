865317ba8c0a76f3d83c3fb2132535c01faa9945
iconium21 <iconium6509@gmail.com>
2024-10-15 19:58:56 +0500

    fixx

--- goofyahh_maths.cpp
+++ goofyahh_maths.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main()
 {
     int a=5,b=0,c=a*b;
-    int d=a/c;
+    int d=a+c;
+    int e=a/c;
     cout<<"Initital Message";
-    cout<<d;
+    cout<<d <<e;
     cout<<"Final Message";
     return 0;
 }
