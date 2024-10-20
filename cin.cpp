ae9ab82b002775f9d7c1da0993312fda41f62e8d
iconium21 <iconium6509@gmail.com>
2024-10-20 12:32:13 +0500

    idk

---
+++ cin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{  
+    char a;
+    int b=0;
+    cout<<"Enter your number";
+    cin >> a;
+    int c = a/b;
+    cout <<c;
+    return 0;
+
+}
