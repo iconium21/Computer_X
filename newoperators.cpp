ede9235401388acbaad6b0598ed6b42ed29648a3
iconium21 <iconium6509@gmail.com>
2024-10-24 15:36:13 +0500

    operators basic cpp

---
+++ newoperators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a=3,b=4,c=0,d=0; // All variables must be initialized! e.g  'int a=1,b=2,c;' is invalid! Set it to 0.
+ c+=b;            // c+=b means: c = c+b 
+ d-=b;            // d-=b means: d = d-b 
+ cout<<"Value of Variable c:\t"<<c <<std::endl;
+ cout<<"Value of Variable d:\t"<<d <<std::endl;
+ return 0;  
+}
