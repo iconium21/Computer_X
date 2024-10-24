00269b3d9a95a6b821cad6d308017e01f002a8cb
iconium21 <iconium6509@gmail.com>
2024-10-24 15:40:35 +0500

    test

--- newoperators.cpp
+++ newoperators.cpp
@@ -4,7 +4,7 @@ int main()
 {
  int a=3,b=4,c=0,d=0; // All variables must be initialized! e.g  'int a=1,b=2,c;' is invalid! Set it to zero
  c+=b;            // c+=b means: c = c+b 
- d-=b;            // d-=b means: d = d-b  
+ d-=b;            // d-=b means: d = d-b  !
  cout<<"Value of Variable c:\t"<<c <<std::endl;
  cout<<"Value of Variable d:\t"<<d <<std::endl;
  return 0;  
