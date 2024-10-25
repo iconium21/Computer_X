df7bf92b916d7262191cee8676d7c5499a5f6d7c
iconium21 <iconium6509@gmail.com>
2024-10-25 16:34:15 +0500

    new

---
+++ datatypes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+int main(){
+    int Integer = 35; // Integer(s)
+    float Float = 3.14; //Floating point number (decimals)
+    char Char = 'A'; //Character can be any single digit character
+    bool Booolean = true; //Boolean can be true or false (1 or 0)
+    string MyString = "Hello";
+    cout << "Integer: " << Integer << std::endl;
+    cout << "Float: " <<  Float << std::endl;
+    cout << "Character: " << Char << std::endl;
+    cout << "Boolean: " <<Booolean << std::endl;
+    return 0;
+}
