8cad9d40885d395a75742a8043f0105935273ddd
iconium21 <iconium6509@gmail.com>
2024-10-29 21:05:31 +0500

    Basic

---
+++ addition_of_stringsNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Adding strings v1.1 \n";
+    cout << "Adding (concatenation) strings a b c. \n";
+    string a="5",b="6";
+    string c = "10";
+    cout << a+b+c << std::endl;
+    return 0;
+
+}
---
+++ length_of_string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout << "Adding strings with numbers wont work (v1)";
+ string a = "10";
+ int b = 5;
+ cout << a+b << std::endl;
+ return 0;
+}
