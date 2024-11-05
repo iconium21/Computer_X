0f34496042b77b2a10daecda0e4f5161c72eb80f
iconium21 <iconium6509@gmail.com>
2024-11-05 15:26:46 +0500

    BASIC

---
+++ if_else.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "==if_else v1.1" << endl;
+    int number;
+    cout << "Enter your Number:  ";
+    cin >> number;
+    if(number>=3)
+    {
+        cout << "You can play this game.\n";
+    }
+    else {
+     cout << "You cannot play this game.\n";
+    }
+    return 0;
+    
+}
+    
