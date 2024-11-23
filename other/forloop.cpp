841cdd98bc734bd5654cfa5fd90cf0cb67b0adcd
Computer_X <iconium21@gmail.com>
2024-11-23 20:46:11 +0500

    test

---
+++ other/forloop.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <ostream>
+int main(){
+    int i;
+    std::cout<<"hello\n";
+    for (i=0; i<5; i++) {
+        std::cout<<i<<std::endl;
+    }
+    return 0;
+}
---
+++ other/testhelloworld.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hellow world23\n";
+    return 0;
+}
